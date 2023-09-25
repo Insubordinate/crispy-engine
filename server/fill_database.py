af75412b6b3ae1f36141ee5501a5657c743f5549
AXL CRUZ <axl.cruz@doubleverify.com>
2023-09-25 15:51:45 -0400

    Refactored and reformatted

--- server/fill_database.py
+++ server/fill_database.py
@@ -1,11 +1,29 @@
+"""Used to fill the database for tests"""
 import sqlite3 as sql
 
 
-with sql.connect('database.db') as con:
+with sql.connect("database.db") as con:
     cur = con.cursor()
-    cur.execute('DROP TABLE IF EXISTS projects')
-    cur.execute("""CREATE TABLE IF NOT EXISTS projects (id integer PRIMARY KEY,name text NOT NULL,email NOT NULL); """)
+    cur.execute("DROP TABLE IF EXISTS projects")
+    cur.execute(
+        """CREATE TABLE IF NOT EXISTS projects 
+        (
+            id integer PRIMARY KEY,
+            name text NOT NULL,
+            email NOT NULL
+        );
+        """
+    )
 
-    cur.execute(" INSERT into projects (name,email) VALUES (?,?)",('bob','something@multiverse.com'))
-    cur.execute(" INSERT into projects (name,email) VALUES (?,?)", ('bill','notsomething@multiverse.com'))
-    cur.execute(" INSERT into projects (name,email) VALUES (?,?)", ('ted','definitelysomething@multiverse.com'))
+    cur.execute(
+        " INSERT into projects (name,email) VALUES (?,?)",
+        ("bob", "something@multiverse.com"),
+    )
+    cur.execute(
+        " INSERT into projects (name,email) VALUES (?,?)",
+        ("bill", "notsomething@multiverse.com"),
+    )
+    cur.execute(
+        " INSERT into projects (name,email) VALUES (?,?)",
+        ("ted", "definitelysomething@multiverse.com"),
+    )
--- server/hello.py
+++ server/hello.py
@@ -1,261 +1,231 @@
+"""Used to spin up the server """
 import sqlite3 as sql
-
 import flask
-from flask import Flask,request,jsonify
-from flask_cors import CORS,cross_origin
-import json
-
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 cors = CORS(app)
 
 
+def connect_to_db():
+    """Opens a connection to the database"""
+    con = sql.connect("database.db")
+    cur = con.cursor()
+    return con, cur
 
-@app.route("/user",methods=['POST','DELETE','GET','PUT'])
+
+def select_all_from_db(column, value, cursor):
+    """Returns all objects from a db with only 1 filter"""
+    cursor.execute(f"select * from projects where {column}=?", (value,))
+    return cursor.fetchall()
+
+
+def unpack_data(data, cursor):
+    """unpacks the data object"""
+    results = []
+    columns = [column[0] for column in cursor.description]
+    for row in data:
+        results.append(dict(zip(columns, row)))
+    return results
+
+
+def add_user_to_db(values, con, cursor):
+    """Adds a user to the database"""
+    cursor.execute(
+        "INSERT INTO PROJECTS (email,name) VALUES (?,?)", (values[0], values[1])
+    )
+    con.commit()
+
+
+def delete_user_from_db(email, con, cur):
+    """Deletes a user from the database"""
+    cur.execute("delete form projects where email =?", (email,))
+    con.commit()
+
+
+def handle_post_request():
+    """Handles the post request"""
+    if request.args.get("email") is None or request.args.get("name") is None:
+        return {"status": "failure", "error": "This was unable to be registered"}
+
+    email = request.args.get("email")
+    name = request.args.get("name")
+
+    con, cur = connect_to_db()
+    data = select_all_from_db("email", email, cur)
+    data = unpack_data(data, cur)
+
+    if data:
+        return {"status": "failure", "error": "This email is already registered"}
+
+    add_user_to_db([email, name], con, cur)
+
+    data = select_all_from_db("email", email, cur)
+    data = unpack_data(data, cur)
+
+    con.close()
+
+    if not data:
+        return {
+            "status": "failure",
+            "error": "This process was unable to be completed",
+        }
+
+    return {"status": "success", "response": data}
+
+
+def handle_delete_request():
+    """Handles the case where the server receives a delete request"""
+
+    if request.args.get("email") is None:
+        return {"status": "failure", "error": "Missing Fields"}
+
+    email = request.args.get("email")
+    con, cur = connect_to_db()
+    data = select_all_from_db("email", email, cur)
+    data = unpack_data(data, cur)
+
+    if not data:
+        return {"status": "failure", "error": "Email not found"}
+
+    delete_user_from_db(email, con, cur)
+    data = select_all_from_db("email", email, cur)
+    data = unpack_data(data, cur)
+
+    if not data:
+        return {"status": "success", "message": "user deleted"}
+
+    return {"status": "failure", "error": "user not able to be deleted"}
+
+
+def handle_get_request():
+    """Handling the serve getting a get request"""
+    is_empty = True
+
+    try:
+        email = request.args.get("email")
+        if email is not None:
+            process_message = "email given"
+            is_empty = False
+        else:
+            process_message = "no email given"
+    except TypeError:
+        pass
+
+    con, cur = connect_to_db()
+
+    if is_empty:
+        cur.execute("""SELECT * FROM PROJECTS""")
+
+        data = cur.fetchall()
+        data = unpack_data(data, cur)
+        con.close()
+
+        if not data:
+            return {
+                "status": "failure",
+                "error": "This data was not able to be returned",
+            }
+        return jsonify(
+            {
+                "response": data,
+                "status": "success",
+                "body_status": process_message,
+            }
+        )
+
+    data = select_all_from_db("email", email, cur)
+    data = unpack_data(data, cur)
+    con.close()
+    if not data:
+        return {
+            "status": "failure",
+            "error": "This user data was not able to be returned",
+        }
+    return jsonify(
+        {
+            "response": data,
+            "status": "success",
+            "body_status": process_message,
+        }
+    )
+
+
+def handle_update_email(cur, con, new_name, email):
+    """handles updating the email"""
+
+    cur.execute("""UPDATE PROJECTS SET name=? WHERE email=?""", (new_name, email))
+    con.commit()
+    data = select_all_from_db("email", email, cur)
+    data = unpack_data(data, cur)
+    con.close()
+
+    if data[0]["name"] != new_name:
+        return {"status": "failure", "error": "unable to update user "}
+    return {"status": "success", "message": "user successfully edited"}
+
+
+def handle_put_request():
+    """Server handling the put request from a user"""
+    if (
+        request.args.get("email") is None
+        or request.args.get("name") is None
+        or request.args.get("newName") is None
+        or request.args.get("newEmail") is None
+    ):
+        return {"status": "failure", "error": "Missing Fields"}
+
+    email = request.args.get("email")
+    name = request.args.get("name")
+    new_name = request.args.get("newName")
+    new_email = request.args.get("newEmail")
+
+    con, cur = connect_to_db()
+    cur.execute("""SELECT * FROM PROJECTS where email=? and name=?""", (email, name))
+    data = cur.fetchall()
+    data = unpack_data(data, cur)
+    if not data:
+        con.close()
+        return {"status": "failure", "error": "this user does not exist"}
+
+    if new_email == email:
+        handle_update_email(cur, con, new_name, email)
+
+    cur.execute("""UPDATE PROJECTS SET name=? WHERE email=?""", (new_name, email))
+    con.commit()
+    data = select_all_from_db("email", email, cur)
+    data = unpack_data(data, cur)
+
+    if data:
+        con.close()
+        return {
+            "status": "failure",
+            "error": "the new email is already registered",
+        }
+
+    cur.execute(
+        """UPDATE PROJECTS SET name=?,email=? WHERE email=?""",
+        (new_name, new_email, email),
+    )
+    con.commit()
+    data = select_all_from_db("email", email, cur)
+    data = unpack_data(data, cur)
+    con.close()
+    if not data:
+        return {"status": "failure", "error": "unable to update user "}
+    return {"status": "success", "message": "user successfully added"}
+
+
+@app.route("/user", methods=["POST", "DELETE", "GET", "PUT"])
 @cross_origin()
 def user():
-
-
-   if flask.request.method == 'POST':
-
-      """ Add A User to the database"""
-
-      """Ccheck to see if request is empty"""
-      if request.args.get('email') is None or request.args.get('name') is None :
-         return {'status': 'failure', 'error': 'This was unable to be registered'}
-
-
-      email=request.args.get('email')
-      name=request.args.get('name')
-
-      """Check to see if email already used"""
-
-      con = sql.connect("database.db")
-      cur = con.cursor()
-
-
-      cur.execute("""SELECT * FROM PROJECTS where email=?""",(email,))
-      data = cur.fetchall()
-      results=[]
-      columns = [column[0] for column in cur.description]
-      for row in data:
-         results.append(dict(zip(columns, row)))
-      if data != []:
-         return {'status': 'failure', 'error': 'This email is already registered'}
-
-
-
-
-      cur.execute("""INSERT INTO PROJECTS (name,email) VALUES (?,?)""",(name,email))
-      con.commit()
-
-      cur.execute("""SELECT * FROM PROJECTS where email=?""",(email,))
-      data = cur.fetchall()
-      con.close()
-
-
-      results=[]
-      columns = [column[0] for column in cur.description]
-      for row in data:
-         results.append(dict(zip(columns, row)))
-      con.close()
-
-
-      if data == []:
-         return {'status': 'failure', 'error': 'This process was unable to be completed'}
-
-      return {'status': 'success','response':results}
-
-
-
-
-   elif flask.request.method == 'DELETE':
-
-      """ Delete A User from the database"""
-
-
-      """Check to see if request is empty"""
-      if request.args.get('email') is None:
-         return {'status': 'failure', 'error': 'Missing Fields'}
-
-      email=request.args.get('email')
-
-      """Check to see if email exists """
-
-      con = sql.connect("database.db")
-      cur = con.cursor()
-
-
-      cur.execute("""SELECT * FROM PROJECTS where email=?""" ,(email,))
-      data = cur.fetchall()
-      results=[]
-      columns = [column[0] for column in cur.description]
-      for row in data:
-         results.append(dict(zip(columns, row)))
-      if data == []:
-         return {'status': 'failure', 'error': 'Email not found'}
-
-
-
-
-      cur.execute("""DELETE from PROJECTS WHERE email=?""",(email,))
-      con.commit()
-
-
-
-
-      cur.execute("""SELECT * FROM PROJECTS where email=?""" ,(email,))
-      data = cur.fetchall()
-      results=[]
-      columns = [column[0] for column in cur.description]
-      for row in data:
-         results.append(dict(zip(columns, row)))
-
-
-      if data == []:
-         return {'status': 'success', 'message':'user deleted'}
-
-      return {'status': 'failure', 'error':'user not able to be deleted'}
-
-
-   elif flask.request.method == 'GET':
-
-      """Return a single user or all users"""
-
-      isEmpty=True
-
-      try:
-         email=request.args.get('email')
-         if email is not None:
-            process_message='email given'
-            isEmpty=False
-         else:
-            process_message = "no email given"
-      except:
-         pass
-
-
-      con = sql.connect("database.db")
-      cur = con.cursor()
-
-
-
-
-      if isEmpty:
-         cur.execute("""SELECT * FROM PROJECTS""")
-         data = cur.fetchall()
-         columns = [column[0] for column in cur.description]
-         results = []
-         for row in data:
-            results.append(dict(zip(columns, row)))
-         con.close()
-         if data == []:
-            return {'status': 'failure', 'error': 'This data was not able to be returned'}
-         return jsonify({"response": results, 'status': 'success','body_status':process_message})
-
-      else:
-         cur.execute("""SELECT * FROM PROJECTS where email=?""", (email,))
-         data = cur.fetchall()
-         columns = [column[0] for column in cur.description]
-         results = []
-         for row in data:
-            results.append(dict(zip(columns, row)))
-         con.close()
-         if data == []:
-            return {'status': 'failure', 'error': 'This user data was not able to be returned'}
-         return jsonify({"response":results,'status':'success','body_status':process_message})
-
-   elif flask.request.method == 'PUT':
-
-      """Update a User"""
-
-
-
-      """Check to see if request is empty"""
-      if request.args.get('email') is None or request.args.get('name') is None or request.args.get('newName') is None or request.args.get('newEmail') is None:
-         return {'status': 'failure', 'error': 'Missing Fields'}
-
-
-      email = request.args.get('email')
-      name = request.args.get('name')
-      newName = request.args.get('newName')
-      newEmail = request.args.get('newEmail')
-
-
-
-      con = sql.connect("database.db")
-      cur = con.cursor()
-
-      """Check to see if old Entry Exists"""
-
-      cur.execute("""SELECT * FROM PROJECTS where email=? and name=?""", (email,name))
-      data = cur.fetchall()
-      results = []
-      columns = [column[0] for column in cur.description]
-      for row in data:
-         results.append(dict(zip(columns, row)))
-      if data == []:
-         con.close()
-         return {'status': 'failure', 'error': 'this user does not exist'}
-
-
-
-      """Check to see if only name is being updated"""
-      if newEmail == email:
-
-         """Update the name"""
-
-         cur.execute("""UPDATE PROJECTS SET name=? WHERE email=?""", (newName,email))
-         con.commit()
-         cur.execute("""SELECT * FROM PROJECTS where email=?""", (email,))
-         data = cur.fetchall()
-         con.close()
-
-         results = []
-         columns = [column[0] for column in cur.description]
-         for row in data:
-            results.append(dict(zip(columns, row)))
-
-         """Check to see if username was updated"""
-         if results[0]['name']!=newName:
-            return {'status': 'failure', 'error': 'unable to update user '}
-         else:
-            return {'status':'success','message':'user successfully edited'}
-
-      else:
-
-         """Check to see that new email doesn't already exist"""
-
-         cur.execute("""SELECT * FROM PROJECTS where email=?""", (newEmail,))
-         data = cur.fetchall()
-         results = []
-         columns = [column[0] for column in cur.description]
-         for row in data:
-            results.append(dict(zip(columns, row)))
-         if data != []:
-            con.close()
-            return {'status': 'failure', 'error': 'the new email is already registered'}
-
-
-         """Update the User"""
-
-         cur.execute("""UPDATE PROJECTS SET name=?,email=? WHERE email=?""", (newName,newEmail,email))
-         con.commit()
-         cur.execute("""SELECT * FROM PROJECTS where email=?""", (newEmail,))
-         data = cur.fetchall()
-         con.close()
-
-         results = []
-         columns = [column[0] for column in cur.description]
-         for row in data:
-            results.append(dict(zip(columns, row)))
-
-         """Check to see if username was updated"""
-         if data==[]:
-            return {'status': 'failure', 'error': 'unable to update user '}
-         else:
-            return {'status': 'success', 'message': 'user successfully added'}
-
-
-
-
+    """Add A User to the database"""
+    if flask.request.method == "POST":
+        handle_post_request()
+    if flask.request.method == "DELETE":
+        handle_delete_request()
+    if flask.request.method == "GET":
+        handle_get_request()
+    if flask.request.method == "PUT":
+        handle_put_request()
